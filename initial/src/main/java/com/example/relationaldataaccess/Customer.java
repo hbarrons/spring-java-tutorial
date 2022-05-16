2fea277c44779b57b079d4275729326ffe296d9a
hbarrons <hbarrons@gmail.com>
2022-05-16 19:15:00 -0400

    stub up customer object

---
+++ initial/src/main/java/com/example/relationaldataaccess/Customer.java
@@ -0,0 +1,19 @@
+package com.example.relationaldataaccess;
+
+public class Customer {
+  private long id;
+  private String firstName, lastName;
+
+  public Customer (long id, String firstname, String lastName) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName
+  }
+
+  @Override
+  public String toString() {
+    return String.format(
+      "Customer[id=%d, firstName='%s', lastName='%s']",
+      id, firstName, lastName);
+  }
+}
